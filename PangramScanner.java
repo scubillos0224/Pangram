bd30b754486f42e00e2b819900e06eae291df942
Sebastian Cubillos <71351950+scubillos0224@users.noreply.github.com>
2023-03-02 09:54:18 -0500

    Add files via upload

---
+++ PangramScanner.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class PangramScanner {
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        System.out.println("Please enter any sentence you would like to check for a pangram.");
+       
+        
+        
+        //String s = "The quick brown fox jumps over the lazy dog";
+
+        boolean[] mark = new boolean[26];
+        int index = 0;
+        boolean pangram = true;
+
+        for (int i = 0; i < s.length(); i++) {
+
+            char cur = s.charAt(i);
+
+            if (cur >= 'A' && cur <= 'Z') {
+                index = cur - 'A';
+            } else if (cur >= 'a' && cur <= 'z') {
+                index = cur - 'a';
+            }
+            mark[index] = true;
+        }
+        for (int i = 0; i < mark.length; i++) {
+            if (mark[i] == false) {
+                pangram = false;
+            }
+
+        }
+        if (pangram) {
+            System.out.println("You have a pangram!");
+        } else {
+            System.out.println("Sorry, this is not a pangram.");
+        }
+    }
+}
